1edf3bf9c7b45ef8cd49648cb65c6acfb420a1a2
Bill Davis <bdavisx@yahoo.com>
2015-08-27 10:47:08 -0500

    Playing around with some settings and stuff.

--- JMonkeyEngineTest1/src/main/java/com/tartner/jmetest/JMETest1.kt
+++ JMonkeyEngineTest1/src/main/java/com/tartner/jmetest/JMETest1.kt
@@ -7,14 +7,15 @@ import com.jme3.math.Vector3f
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
 import com.jme3.scene.shape.Sphere
+import com.jme3.system.AppSettings
+import java.awt.GraphicsEnvironment
 
 public class Main : SimpleApplication() {
 
     override fun simpleInitApp() {
+
         val box = Box(Vector3f.ZERO, 1f, 1f, 1f);
-
         val geometry = Geometry("Box", box);
-
         // create a simple blue material
         val material = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         material.setColor("Color", ColorRGBA.Blue);
@@ -26,8 +27,8 @@ public class Main : SimpleApplication() {
         val vector2 = Vector3f(2.0f, 1.0f, -3.0f);
         val shape = Sphere(5,10,5f);
         val geometry2 = Geometry("Box", shape);
-        val material2 = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material2.setColor("Color", ColorRGBA.Green);
+        val material2 = Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
+//        material2.setColor("Color", ColorRGBA.Green);
         geometry2.setMaterial(material2);
         geometry2.setLocalTranslation(vector2);
         rootNode.attachChild(geometry2);
@@ -36,6 +37,24 @@ public class Main : SimpleApplication() {
 }
 
 fun main(args: Array<String>) {
+//    val device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+//    val modes = device.getDisplayModes();
+
+    val settings = AppSettings(true)
+    settings.setBitsPerPixel(32)
+    settings.setFrameRate(60)
+    settings.setFrequency(60)
+
+//    settings.setFullscreen(true)
+//    settings.setWidth(1600)
+//    settings.setHeight(900)
+
+    settings.setFullscreen(false)
+    settings.setWidth(1024)
+    settings.setHeight(768)
+
     val main = Main()
+    main.setShowSettings(false)
+    main.setSettings(settings)
     main.start()
 }
